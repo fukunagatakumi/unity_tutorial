6808ab22ab67b00b8931a8aa1e95a180ea117e69
fukunagatakumi <thekomoe777@gmail.com>
2019-05-02 13:34:46 +0900

    キューブに対する物理特性

---
+++ Assets/Ballcontroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballcontroller : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var force = (transform.forward + transform.right) * speed;
+        GetComponent<Rigidbody>().AddForce(force, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
