838ccef7f82de6b823974eea8475576c51df978d
fukunagatakumi <thekomoe777@gmail.com>
2019-05-02 14:03:02 +0900

    ブロックの追加

---
+++ Assets/Blockcontroller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockcontroller : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
